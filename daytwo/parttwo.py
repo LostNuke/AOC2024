b7dffbdb476a365dbafce68a9c4de46e8f42e042
LostNuke <nroostaazad@gmail.com>
2024-12-05 23:27:49 +0100

    Terrible Python code

--- daytwo/parttwo.py
+++ daytwo/parttwo.py
@@ -1,12 +1,12 @@
 file = open('./input', 'r')
-safes = 0
-for line in file:
-    nums = line.split(' ')
+
+def check_line_without_number(nums, remove=None) -> bool:
+    if remove is not None: nums.pop(remove)
     incdec = 0
     safe = True
     for index, num in enumerate(nums):
         if len(nums) == index+1: break
-        diff = abs(int(num) - int(nums[index+1]))
+        diff = abs(int(num.replace("\n", "")) - int(nums[index+1].replace("\n", "")))
         if 1 <= diff <= 3:
             if incdec == 0: incdec = int(num) - int(nums[index+1])
             elif incdec > 0: # Descending
@@ -20,6 +20,20 @@ for line in file:
         else:
             safe = False
             break
-    if safe: safes += 1
+
+    return safe
+
+safes = 0
+for line in file:
+    nums = line.replace("\n","").split(' ')
+    safe = check_line_without_number(nums)
+    if safe:
+        safes += 1
+        continue
+    for index, num in enumerate(nums):
+        safe =  check_line_without_number(nums.copy(), index)
+        if safe:
+            safes += 1
+            break
 
 print(safes)
